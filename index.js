d9d42041b10c5c46f2f1383e1b99bf3b6d3ef74c
Sakshi Bagde <86615437+Sakshi-z@users.noreply.github.com>
2023-02-25 11:04:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+function clearErrors(){
+    
+    errors = document.getElementsByClassName('formerror');
+    for(let item of errors)
+    {
+       item.innerHTML= "";
+    }
+}
+function seterror(id, error){
+    element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = error;
+
+}
+function validateForm(){
+    var returnval = true;
+    clearErrors();
+  var name = document.forms['myForm']["fname"].value;
+  if(name.length<5){
+    seterror("name","*Length of name is too short!")
+    returnval = false;
+  }
+  var Email = document.forms['myForm']["femail"].value;
+  if(Email.length>30){
+    seterror("email","*Email length is too long!")
+    returnval = false;
+  }
+  var phone = document.forms['myForm']["fphone"].value;
+  if(phone.length!=10){
+    seterror("phone","*Phone number should be of 10 digits!")
+    returnval = false;
+  }
+  var password = document.forms['myForm']["fpass"].value;
+  if(password.length<6){
+    seterror("pass","*Password shoulb be 6 characters long!")
+    returnval = false;
+  }
+  var confirmpassword = document.forms['myForm']["fcpass"].value;
+  if(confirmpassword!=password){
+    seterror("cpass","*Password and Confirm password should match!")
+    returnval = false;
+  }
+  return returnval;
+}
